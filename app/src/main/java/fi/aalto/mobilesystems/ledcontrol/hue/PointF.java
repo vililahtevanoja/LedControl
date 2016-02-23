dd9072e3be65549e4f1814a0937de428221b91ea
Vili Lähtevänoja <vili.lahtevanoja@gmail.com>
2016-02-23 18:26:01 +0200

    Added PointF class for point representation
    The class extends com.philips.lighting.hue.sdk.utilities.impl.PointF

---
+++ app/src/main/java/fi/aalto/mobilesystems/ledcontrol/hue/PointF.java
@@ -0,0 +1,15 @@
+package fi.aalto.mobilesystems.ledcontrol.hue;
+
+/**
+ * Class for a representing a point
+ */
+public class PointF extends com.philips.lighting.hue.sdk.utilities.impl.PointF {
+    public PointF(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean equals(PointF p) {
+        return (this.x == p.x) && (this.y == p.y);
+    }
+}
