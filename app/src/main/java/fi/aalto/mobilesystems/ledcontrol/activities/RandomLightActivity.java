fa8dc10bbe72ea4f7a8f3e39fa208cb71b430c67
harins <harins2011@gmail.com>
2016-04-25 23:28:14 +0300

    UI updated

--- app/src/main/java/fi/aalto/mobilesystems/ledcontrol/activities/RandomLightActivity.java
+++ app/src/main/java/fi/aalto/mobilesystems/ledcontrol/activities/RandomLightActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.philips.lighting.hue.sdk.PHHueSDK;
@@ -18,6 +19,13 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
 import fi.aalto.mobilesystems.ledcontrol.R;
 
 
@@ -27,7 +35,7 @@ public class RandomLightActivity extends AppCompatActivity {
 
     int DELAY;
     CheckBox ShotCheckBox;
-    Button StartBtn, CancelBtn;
+    Button StartBtn, CancelBtn, StartDisko, StopDisko;
     EditText EtTime;
     Timer timer;
     RandomBlinkLight task;
@@ -39,7 +47,6 @@ public class RandomLightActivity extends AppCompatActivity {
         ShotCheckBox = (CheckBox) findViewById(R.id.shot);
         EtTime = (EditText) findViewById(R.id.edt_time);
         StartBtn = (Button) findViewById(R.id.start);
-        CancelBtn = (Button) findViewById(R.id.cancel);
         StartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
@@ -67,8 +74,43 @@ public class RandomLightActivity extends AppCompatActivity {
                 }
             }
         });
-
+        CancelBtn = (Button) findViewById(R.id.cancel);
         CancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (timer != null) {
+                    timer.cancel();
+                    timer = null;
+                }
+            }
+        });
+
+        StartDisko = (Button) findViewById(R.id.btnDiskoOn);
+        StartDisko.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
+
+                PitchDetectionHandler pdh = new PitchDetectionHandler() {
+                    @Override
+                    public void handlePitch(PitchDetectionResult result,AudioEvent e) {
+                        final float pitchInHz = result.getPitch();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView text = (TextView) findViewById(R.id.soundHertz);
+                                text.setText("" + pitchInHz);
+                            }
+                        });
+                    }
+                };
+                AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+                dispatcher.addAudioProcessor(p);
+                new Thread(dispatcher,"Audio Dispatcher").start();
+            }
+        });
+        StopDisko = (Button) findViewById(R.id.btnDiskoOff);
+        StopDisko.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (timer != null) {
@@ -107,6 +149,30 @@ public class RandomLightActivity extends AppCompatActivity {
             PHLightState lightState = new PHLightState();
             lightState.setHue(rand.nextInt(MAX_HUE));
             this.sdk.getSelectedBridge().updateLightState(light, lightState);
+        }
+    }
+
+    public void randomLights(float pitchInHz ) {
+        this.sdk = PHHueSDK.getInstance();
+        if(this.sdk.getSelectedBridge() == null) {
+            Log.e("Random Light", "Bridge is null");
+            return;
+        }
+
+        List<PHLight> allLights = this.sdk.getSelectedBridge().getResourceCache().getAllLights();
+        Random rand = new Random();
+        if(pitchInHz > 10 || pitchInHz < 100) {
+            for (PHLight light : allLights) {
+                PHLightState lightState = new PHLightState();
+                lightState.setHue(rand.nextInt(MAX_HUE));
+                this.sdk.getSelectedBridge().updateLightState(light, lightState);
+            }
+        }else if(pitchInHz > 100 || pitchInHz < 200) {
+            for (PHLight light : allLights) {
+                PHLightState lightState = new PHLightState();
+                lightState.setHue(rand.nextInt(MAX_HUE));
+                this.sdk.getSelectedBridge().updateLightState(light, lightState);
+            }
         }
     }
 }
